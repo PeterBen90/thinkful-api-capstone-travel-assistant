d8f62f17e085dd8349a9ceddcaf68296dcdeb33c
Peter Benvenuto <peterbenvenuto90@gmail.com>
2017-12-29 03:08:31 +0000

    HTML update

---
+++ index.js
@@ -0,0 +1,3 @@
+const WEATHER_URL = `https://api.openweathermap.org/data/2.5/forecast?id=524901`
+const FOURSQUARE_URL = `https://api.foursquare.com/v2/venues/explore?&client_id=02S31L2GILSBQDIMCCDQS5YYMWJW4PZRA0DN2NIZDYZUNC3I&client_secret=B2SUFTUSBX2D5W2UVXN4BVAT1WYUKTIP1MHZNQZWWIYBEGQ2&v=20171228`
+
