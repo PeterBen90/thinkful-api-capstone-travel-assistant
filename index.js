ca97a6103e7d6e3ea85cb4cceec0b61998c91426
Peter Benvenuto <peterbenvenuto90@gmail.com>
2018-01-03 16:53:58 +0000

    Functionality complete

--- index.js
+++ index.js
@@ -1,3 +1,68 @@
-const WEATHER_URL = `https://api.openweathermap.org/data/2.5/forecast?id=524901`
-const FOURSQUARE_URL = `https://api.foursquare.com/v2/venues/explore?&client_id=02S31L2GILSBQDIMCCDQS5YYMWJW4PZRA0DN2NIZDYZUNC3I&client_secret=B2SUFTUSBX2D5W2UVXN4BVAT1WYUKTIP1MHZNQZWWIYBEGQ2&v=20171228`
+function getWeatherResults() {
+  $('.js-search-form').submit(event => {
+    event.preventDefault();
+    let city = $('.search-query').val();
+    if (city !== '') {
 
+      $.ajax({
+
+       url: `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=7113abbcc3d0ba25223772c94f923152`,
+       type: 'GET',
+       dataType: 'jsonp',
+       success: function(data) {
+         let widget = showWeatherData(data);
+
+         $('.js-weather-results').html(widget);
+         //$('.search-query').val('');
+       }
+
+
+      });
+
+    } else {
+      $("#error").html("Field cannot be empty!");
+    }
+
+ });
+}
+
+function showWeatherData(data) {
+  return `
+    <h3>Current Weather: ${data.weather[0].main}</h3>
+    <h3>Description: ${data.weather[0].description}</h3>
+    <h3>Tempurature: ${data.main.temp} °C</h3>
+  `;
+}
+
+function getPlacesResults() {
+  $('.category-button').click(function(event){
+    let category = $(this).text();
+    let city = $('.search-query').val();
+
+    $.ajax({
+
+      url: `https://api.foursquare.com/v2/venues/explore?&near=${city}&section=${category}&venuePhotos=1&limit=12&client_id=02S31L2GILSBQDIMCCDQS5YYMWJW4PZRA0DN2NIZDYZUNC3I&client_secret=B2SUFTUSBX2D5W2UVXN4BVAT1WYUKTIP1MHZNQZWWIYBEGQ2&v=20180103`,
+      type: 'GET',
+      dataType: 'jsonp',
+      success: function(data) {
+        console.log(data);
+         let results = data.response.groups[0].items.map(function (item, index) {
+          return showPlacesData(item);
+         });
+         $('.js-results').html(results);
+      }
+
+    });
+
+  });
+}
+
+function showPlacesData(result) {
+  return `
+    <p>${result.venue.url}</p>
+  `
+}
+
+
+$(getWeatherResults);
+$(getPlacesResults);
