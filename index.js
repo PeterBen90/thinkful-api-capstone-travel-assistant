8cdfc1fe961df0fece9c255ff9c70aff32451f9f
Peter Benvenuto <peterbenvenuto90@yahoo.ca>
2018-02-07 21:13:50 -0500

    fixed results

--- index.js
+++ index.js
@@ -92,15 +92,20 @@ function getPlacesResults() {
 //show places data on DOM
 function showPlacesData(result) {
   return `
-    <div class="col-4">
+     <div class="col-4">
       <div class="venue-results">
-      <img class="venue-photo" src="https://igx.4sqi.net/img/general/width333${result.venue.featuredPhotos.items[0].suffix}" alt="photo from venue">
+      <img class="venue-photo" src="https://igx.4sqi.net/img/general/width960${result.venue.featuredPhotos.items[0].suffix}" alt="photo from venue">
         <div class="results-content">
-          <h3 class="venue"><a href="${result.venue.url}" target="_blank">${result.venue.name}</a></h3>
-          <p>Type of venue: ${result.venue.categories[0].name}</p>
-          <p>Phone Number: ${result.venue.contact.phone}</p>
-          <p>${result.venue.location.formattedAddress[0]}</p>
-          <p>${result.venue.location.formattedAddress[1]}</p>
+                <h2 class="result-name"><a href="${result.venue.url}" target="_blank">${result.venue.name}</a></h2>
+                <span class="icon">
+                    <img src="${result.venue.categories[0].icon.prefix}bg_32${result.venue.categories[0].icon.suffix}" alt="category-icon">
+                </span>
+                <span class="icon-text">
+                    ${result.venue.categories[0].name}
+                </span>
+                <p class="result-address">${result.venue.location.formattedAddress[0]}</p>
+                <p class="result-address">${result.venue.location.formattedAddress[1]}</p>
+                <p class="result-address">${result.venue.location.formattedAddress[2]}</p>
         </div>
       </div>
     </div>
